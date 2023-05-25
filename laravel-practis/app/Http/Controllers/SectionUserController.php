623cbb8073b02ff8c02616e61aaf409415dce513
Kosuke Shibuya <github@shibuyakosuke.com>
2023-05-25 11:21:16 +0900

    Feature/section route (#10)
    * add sections
    
    * mod sections
    
    * wip
    
    * wip
    
    * fixer
    
    ---------
    
    Co-authored-by: 渋谷 康介 <shibuya.kosuke@C100088A.local>
    Co-authored-by: 渋谷 康介 <shibuya.kosuke@C100088A.lan>

--- laravel-practis/app/Http/Controllers/SectionUserController.php
+++ laravel-practis/app/Http/Controllers/SectionUserController.php
@@ -5,14 +5,11 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreSectionUserRequest;
 use App\Models\Section;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class SectionUserController extends Controller
 {
-    /**
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function store(StoreSectionUserRequest $request, Section $section)
+    public function store(StoreSectionUserRequest $request, Section $section): RedirectResponse
     {
         $section->users()->attach($request->user_id);
 
@@ -21,10 +18,7 @@ class SectionUserController extends Controller
         return redirect()->route('companies.sections.show', compact('company', 'section'));
     }
 
-    /**
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function destroy(Section $section, User $user)
+    public function destroy(Section $section, User $user): RedirectResponse
     {
         $section->users()->detach($user->id);
 
